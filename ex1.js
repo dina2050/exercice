fb4032f8529f426487d1e55e431d3b85469d2f5d
zerstorers <lemaire-maxime@hotmail.fr>
2019-09-09 15:15:42 +0200

    exercice

--- ex1.js
+++ ex1.js
@@ -1,2 +1,23 @@
-var x = "Bonjour";
-alert(x);
+// Exercice 1
+
+// var x = "Bonjour";
+// alert(x);
+
+// Exercice 2
+
+// var y ="Nom : Doe";
+// var x ="Prénom : John ";
+// var z ="Ville : New York";
+// alert(y+x+z)
+
+// Exercice 3
+
+// var x = ("Bonjour, votre Prénom ?")
+// prompt(x);
+
+// Exercice 4
+
+var x = getElementById("name");
+var y = getElementById("surname");
+var z = getElementById("ville");
+prompt(x + y + z);
